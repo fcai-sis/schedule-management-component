84a5c5df0575c659ac9deb72d4b3738bf30658be
Sherif <sherif3hassan@gmail.com>
2024-02-25 19:20:50 +0200

    Add lecture-related handlers and middlewares

---
+++ src/features/schedules/lecture-logic/handlers/deleteLecture.handler.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import LectureModel from "../../data/models/lecture.model";
+
+type HandlerRequest = Request<{ lectureId: string }, {}, {}>;
+
+/**
+ * Delete a Lecture by its id.
+ */
+const handler = async (req: HandlerRequest, res: Response) => {
+  const lectureId = req.params.lectureId;
+
+  const lecture = await LectureModel.findByIdAndDelete(lectureId);
+
+  if (!lecture) {
+    return res.status(404).json({
+      error: {
+        message: "Lecture not found",
+      },
+    });
+  }
+
+  const response = {
+    message: "Lecture deleted successfully",
+    lecture: {
+      ...lecture.toObject(),
+    },
+  };
+
+  return res.status(200).json(response);
+};
+
+const deleteLectureHandler = handler;
+export default deleteLectureHandler;
---
+++ src/features/schedules/lecture-logic/handlers/getLecture.handler.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import LectureModel from "../../data/models/lecture.model";
+
+// get lecture filtered by scheduleId or courseId or instructorId or hallId or slotId
+
+type HandlerRequest = Request<
+  {},
+  {},
+  {
+    scheduleId?: string;
+    courseId?: string;
+    instructorId?: string;
+    hallId?: string;
+    slotId?: string;
+  }
+>;
+
+const handler = async (req: HandlerRequest, res: Response) => {
+  const { scheduleId, courseId, instructorId, hallId, slotId } = req.query;
+
+  const query = {
+    ...(scheduleId && { scheduleId }),
+    ...(courseId && { courseId }),
+    ...(instructorId && { instructorId }),
+    ...(hallId && { hallId }),
+    ...(slotId && { slotId }),
+  };
+
+  const lectures = await LectureModel.find(query)
+    .populate("courseId")
+    .populate("instructorId")
+    .populate("hallId")
+    .populate("slotId")
+    .populate("scheduleId");
+
+  const response = {
+    lectures: lectures.map((lecture) => lecture.toObject()),
+  };
+
+  return res.status(200).json(response);
+};
+
+const getLectureHandler = handler;
+export default getLectureHandler;
