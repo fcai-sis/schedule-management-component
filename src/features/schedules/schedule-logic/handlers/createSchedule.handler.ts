de3f5919d4f11fe4197425ff7116faa6440508ef
Amr Khaled <lewolfje@gmail.com>
2024-04-19 13:22:30 +0200

    remove try-catch block

--- src/features/schedules/schedule-logic/handlers/createSchedule.handler.ts
+++ src/features/schedules/schedule-logic/handlers/createSchedule.handler.ts
@@ -17,36 +17,22 @@ type HandlerRequest = Request<
 const handler = async (req: HandlerRequest, res: Response) => {
   const { description, level, departmentId, semesterId } = req.body;
 
-  try {
-    const schedule = new Schedule({
-      description,
-      level,
-      departmentId,
-      semesterId,
-    });
+  const schedule = new Schedule({
+    description,
+    level,
+    departmentId,
+    semesterId,
+  });
 
-    // Attempt to save the schedule
-    await schedule.save();
+  await schedule.save();
 
-    // If saved successfully, send a success response
-    const response = {
-      message: "Schedule created successfully",
-      schedule: {
-        ...schedule.toObject(),
-      },
-    };
-    return res.status(201).json(response);
-  } catch (error: any) {
-    // If an error occurs during validation, send an error response
-    if (error.name === "ValidationError") {
-      const errorMessage = error.errors[Object.keys(error.errors)[0]].message;
-      return res.status(400).json({ message: errorMessage });
-    } else {
-      // For other unexpected errors, send a generic error response
-      console.error(error);
-      return res.status(500).json({ message: "Failed to create schedule" });
-    }
-  }
+  const response = {
+    message: "Schedule created successfully",
+    schedule: {
+      ...schedule.toObject(),
+    },
+  };
+  return res.status(201).json(response);
 };
 const createScheduleHandler = handler;
 export default createScheduleHandler;
