6a704986cef203add9660b4054eaa9f623631df2
Sherif <sherif3hassan@gmail.com>
2024-04-20 12:33:00 +0200

    Fix typo in import statement in section.routes.ts

--- src/features/schedules/section.routes.ts
+++ src/features/schedules/section.routes.ts
@@ -4,7 +4,7 @@ import { asyncHandler } from "@fcai-sis/shared-utilities";
 import ensureHallAndSlotUniqueMiddleware from "./section-logic/middlewares/ensureHallAndSlotUnique.middleware";
 import createSectionHandler from "./section-logic/handlers/createSection.handler";
 import deleteSectionHandler from "./section-logic/handlers/deleteSection.handler";
-import ensureTaAvailbility from "./section-logic/middlewares/ensureTAAvailbility.middleware";
+import ensureTaAvailbility from "./section-logic/middlewares/ensureTaAvailbility.middleware";
 
 
 const sectionsRoutes = (router: Router) => {
