07b783a501c04ccb668c883f50043ff331d555e6
Sherif <sherif3hassan@gmail.com>
2024-04-20 12:05:50 +0200

    Refactor section.routes.ts to remove unused imports and update middleware names

--- src/features/schedules/section.routes.ts
+++ src/features/schedules/section.routes.ts
@@ -3,45 +3,24 @@ import { Router } from "express";
 import { asyncHandler } from "@fcai-sis/shared-utilities";
 import ensureHallAndSlotUniqueMiddleware from "./section-logic/middlewares/ensureHallAndSlotUnique.middleware";
 import createSectionHandler from "./section-logic/handlers/createSection.handler";
-import getSectionHandler from "./section-logic/handlers/getSection.handler";
-import { paginationQueryParamsMiddleware } from "@fcai-sis/shared-middlewares";
-import getPaginatedSectionHandler from "./section-logic/handlers/getPaginatedSection.handler";
 import deleteSectionHandler from "./section-logic/handlers/deleteSection.handler";
-import getSectionByIdHandler from "./section-logic/handlers/getSectionById.handler";
+import ensureSectionIdInParamsMiddleware from "./section-logic/middlewares/ensureSectionIdInParams.middleware";
 
 
 const sectionsRoutes = (router: Router) => {
     router.post(
         "/",
-        
+
         ensureHallAndSlotUniqueMiddleware,
+
+        ensureSectionIdInParamsMiddleware,
 
         asyncHandler(createSectionHandler)
     );
 
-    router.get(
-        "/",
-
-        asyncHandler(getSectionHandler)
-    );
-
-    router.get(
-        "/read",
-
-        paginationQueryParamsMiddleware,
-
-        asyncHandler(getPaginatedSectionHandler)
-    );
-
-    router.get(
-        "/:sectionId",
-
-        asyncHandler(getSectionByIdHandler)
-    );
-
     router.delete(
         "/:sectionId",
-        
+
         asyncHandler(deleteSectionHandler)
     );
 };
