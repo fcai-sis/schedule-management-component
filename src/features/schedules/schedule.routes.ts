ccc7c8f3d5ef9ffe47cf4acd23b6b8db520b7c16
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-05-08 13:39:00 +0300

    Add new routes

--- src/features/schedules/schedule.routes.ts
+++ src/features/schedules/schedule.routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 
 import { asyncHandler } from "@fcai-sis/shared-utilities";
 
+// TODO: FIX THE IMPORTS (.js ???)
 import createScheduleHandler from "./schedule-logic/handlers/createSchedule.handler.js";
 import getScheduleHandler from "./schedule-logic/handlers/getSchedule.handler.js";
 import updateScheduleHandler from "./schedule-logic/handlers/updateSchedule.handler.js";
+import { Role, checkRole } from "@fcai-sis/shared-middlewares";
+import getCustomTeachingScheduleHandler from "./schedule-logic/handlers/getCustomSchedule.handler.js";
 
 const schedulesRoutes = (router: Router) => {
   router.post(
@@ -17,6 +20,27 @@ const schedulesRoutes = (router: Router) => {
     "/",
 
     asyncHandler(getScheduleHandler)
+  );
+
+  router.get(
+    "/teaching",
+
+    checkRole([
+      Role.INSTUCTOR,
+      Role.TEACHING_ASSISTANT
+    ]),
+
+    asyncHandler(getCustomTeachingScheduleHandler)
+  );
+
+  router.get(
+    "/student",
+
+    checkRole([
+      Role.STUDENT
+    ]),
+
+    asyncHandler(getStudentScheduleHandler)
   );
 
   router.patch("/:scheduleId", asyncHandler(updateScheduleHandler));
