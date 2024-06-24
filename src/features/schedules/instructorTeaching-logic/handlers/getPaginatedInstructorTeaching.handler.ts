c546b9387cbdeeee928a21990c989507be3259c8
Amr Khaled <lewolfje@gmail.com>
2024-06-24 11:29:42 +0300

    use correct pagination variables

--- src/features/schedules/instructorTeaching-logic/handlers/getPaginatedInstructorTeaching.handler.ts
+++ src/features/schedules/instructorTeaching-logic/handlers/getPaginatedInstructorTeaching.handler.ts
@@ -1,56 +1,52 @@
 import { InstructorTeachingModel } from "@fcai-sis/shared-models";
 import { Request, Response } from "express";
 
-
 type HandlerRequest = Request<
-    {},
-    {},
-    {},
-    {
-        instructor?: string;
-        course?: string;
-        semester?: string;
-        skip?: number,
-        limit?: number
-    }
+  {},
+  {},
+  {},
+  {
+    instructor?: string;
+    course?: string;
+    semester?: string;
+    limit?: number;
+    page?: number;
+  }
 >;
 
-
 const handler = async (req: HandlerRequest, res: Response) => {
-    const { instructor, course, semester, skip, limit } = req.query;
-    const page = req.context.page;
-    const pageSize = req.context.pageSize;
+  const { instructor, course, semester } = req.query;
 
-    const query = {
-        ...(instructor && { instructor }),
-        ...(course && { course }),
-        ...(semester && { semester }),
-    };
-    const instructorTeaching = await InstructorTeachingModel.find(
-        query,
-        {
-            _v: 0
+  const query = {
+    ...(instructor && { instructor }),
+    ...(course && { course }),
+    ...(semester && { semester }),
+  };
+  const instructorTeaching = await InstructorTeachingModel.find(
+    query,
+    {
+      _v: 0,
+    },
+    {
+      skip: req.skip ?? 0,
+      limit: req.query.limit as unknown as number,
+    }
+  );
 
-        },
-        {
-            skip: skip ?? 0,
-            limit: limit as unknown as number,
-        }
-    );
+  const count = await InstructorTeachingModel.countDocuments(query);
+  const totalPages = Math.ceil(count / (req.query.limit as unknown as number));
 
-    const count = await InstructorTeachingModel.countDocuments(query);
-    const totalPages = Math.ceil(count / pageSize);
+  const response = {
+    lectures: instructorTeaching.map((instructorTeaching) =>
+      instructorTeaching.toObject()
+    ),
+    count,
+    totalPages,
+    currentPage: req.query.page as unknown as number,
+    pageSize: req.query.limit as unknown as number,
+  };
 
-
-    const response = {
-        lectures: instructorTeaching.map((instructorTeaching) => instructorTeaching.toObject()),
-        count,
-        totalPages,
-        currentPage: page,
-        pageSize: pageSize
-    };
-
-    return res.status(200).json(response);
+  return res.status(200).json(response);
 };
 
 const getPaginatedInstructorTeachingHandler = handler;
