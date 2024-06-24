bb3b4ab633e66d098863b8d6c6a1f7bfcf90b987
Amr Khaled <lewolfje@gmail.com>
2024-06-24 11:30:19 +0300

    refactor to use validateRequestMiddleware

--- src/features/schedules/instructorTeaching-logic/middlewares/createInstructorTeachingValidator.middleware.ts
+++ src/features/schedules/instructorTeaching-logic/middlewares/createInstructorTeachingValidator.middleware.ts
@@ -1,6 +1,5 @@
 import * as validator from "express-validator";
-import { Request, Response, NextFunction } from "express";
-import logger from "../../../../core/logger";
+import { validateRequestMiddleware } from "@fcai-sis/shared-middlewares";
 
 const createInstructorTeachingValidatorMiddleware = [
   validator
@@ -13,15 +12,8 @@ const createInstructorTeachingValidatorMiddleware = [
     .exists()
     .isMongoId()
     .withMessage("courseId must be a valid mongo id"),
-  
-  (req: Request, res: Response, next: NextFunction) => {
-    const errors = validator.validationResult(req);
-    if (!errors.isEmpty()) {
-      logger.error("Validation error", errors.array());
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
+
+  validateRequestMiddleware,
 ];
 
 export default createInstructorTeachingValidatorMiddleware;
