4ddfe66e5bf2c1245fd19a515ea24b8652c2d6f0
Amr Khaled <lewolfje@gmail.com>
2024-06-24 11:43:52 +0300

    put query terms in...query?

--- src/features/schedules/instructorTeaching-logic/handlers/getInstructorTeaching.handler.ts
+++ src/features/schedules/instructorTeaching-logic/handlers/getInstructorTeaching.handler.ts
@@ -1,17 +1,16 @@
 import { InstructorTeachingModel } from "@fcai-sis/shared-models";
 import { Request, Response } from "express";
 
-
 type HandlerRequest = Request<
+  {},
+  {},
+  {},
   {
     instructor?: string;
     course?: string;
     semester?: string;
-  },
-  {},
-  {}
+  }
 >;
-
 
 const handler = async (req: HandlerRequest, res: Response) => {
   const { instructor, course, semester } = req.query;
@@ -23,13 +22,14 @@ const handler = async (req: HandlerRequest, res: Response) => {
   };
 
   const instructorTeaching = await InstructorTeachingModel.find(query)
-  .populate("instructor")
+    .populate("instructor")
     .populate("course")
     .populate("semester");
 
-
   const response = {
-    instructorTeaching: instructorTeaching.map((instructorTeaching) => instructorTeaching.toObject()),
+    instructorTeaching: instructorTeaching.map((instructorTeaching) =>
+      instructorTeaching.toObject()
+    ),
   };
 
   return res.status(200).json(response);
