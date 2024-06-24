cf82b693049295d5bf0c941cfba9f6ddbb85bf64
Amr Khaled <lewolfje@gmail.com>
2024-06-24 11:30:27 +0300

    refactor to use validateRequestMiddleware

--- src/features/schedules/instructorTeaching-logic/middlewares/ensureInstructorTeachingIdInParams.middleware.ts
+++ src/features/schedules/instructorTeaching-logic/middlewares/ensureInstructorTeachingIdInParams.middleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { validateRequestMiddleware } from "@fcai-sis/shared-middlewares";
 import * as validator from "express-validator";
 
 const middlewares = [
@@ -11,21 +11,7 @@ const middlewares = [
     .isMongoId()
     .withMessage("Instructor teaching ID must be a valid Mongo ID"),
 
-  (req: Request, res: Response, next: NextFunction) => {
-    const errors = validator.validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        error: {
-          message: errors.array()[0].msg,
-        },
-      });
-    }
-
-    req.params.instructorTeachingId = req.params.instructorTeachingId.trim();
-
-    next();
-  },
+  validateRequestMiddleware,
 ];
 
 const ensureInstructorTeachingIdInParamsMiddleware = middlewares;
