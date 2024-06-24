b689ed885c010c66ea579e2e4ebd3b8686e15922
Amr Khaled <lewolfje@gmail.com>
2024-06-24 11:29:56 +0300

    apply runValidators when updating

--- src/features/schedules/instructorTeaching-logic/handlers/updateInstructorTeaching.handler.ts
+++ src/features/schedules/instructorTeaching-logic/handlers/updateInstructorTeaching.handler.ts
@@ -1,6 +1,8 @@
-import { InstructorTeachingModel, InstructorTeachingType } from "@fcai-sis/shared-models";
+import {
+  InstructorTeachingModel,
+  InstructorTeachingType,
+} from "@fcai-sis/shared-models";
 import { Request, Response } from "express";
-
 
 type HandlerRequest = Request<
   {
@@ -19,15 +21,20 @@ const handler = async (req: HandlerRequest, res: Response) => {
   const instructorTeachingId = req.params.instructorTeachingId;
   const { instructorTeaching } = req.body;
 
-  const createdInstructorTeaching = await InstructorTeachingModel.findByIdAndUpdate(
-    instructorTeachingId,
-    {
-      ...(instructorTeaching.instructor && { instructor: instructorTeaching.instructor }),
-      ...(instructorTeaching.course && { course: instructorTeaching.course }),
-      ...(instructorTeaching.semester && { semester: instructorTeaching.semester }),
-    },
-    { new: true }
-  );
+  const createdInstructorTeaching =
+    await InstructorTeachingModel.findByIdAndUpdate(
+      instructorTeachingId,
+      {
+        ...(instructorTeaching.instructor && {
+          instructor: instructorTeaching.instructor,
+        }),
+        ...(instructorTeaching.course && { course: instructorTeaching.course }),
+        ...(instructorTeaching.semester && {
+          semester: instructorTeaching.semester,
+        }),
+      },
+      { new: true, runValidators: true }
+    );
 
   if (!createdInstructorTeaching) {
     return res.status(404).json({
