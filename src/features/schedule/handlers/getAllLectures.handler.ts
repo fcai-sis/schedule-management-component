49649e4134b09135ebbb3325fb2522b0cf1341a2
Amr Khaled <lewolfje@gmail.com>
2024-06-29 22:58:39 +0300

    add get all lectures

---
+++ src/features/schedule/handlers/getAllLectures.handler.ts
@@ -0,0 +1,28 @@
+import { LectureModel } from "@fcai-sis/shared-models";
+import { Request, Response } from "express";
+
+/**
+ * Get all available lectures.
+ */
+
+type HandlerRequest = Request;
+
+const getAllLecturesHandler = async (req: HandlerRequest, res: Response) => {
+  const { skip, limit } = req.query;
+
+  const lectures = await LectureModel.find()
+    .populate("course")
+    .populate("hall")
+    .populate("slot")
+    .limit(limit as unknown as number)
+    .skip(Number(skip) ?? 0);
+  console.log(lectures);
+
+  const totalLectures = await LectureModel.countDocuments();
+  return res.status(200).send({
+    lectures,
+    totalLectures,
+  });
+};
+
+export default getAllLecturesHandler;
